e02908417718022deda37624ca017f42ccc52424
allcoolmathgames <habibinfo939@gmail.com>
2025-01-31 15:49:56 +0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,25 +17,22 @@ $(document).ready(function() {
     // Animate the scroll to top
     $('.go-top').click(function(event) {
         event.preventDefault();
-        
         $('html, body').animate({scrollTop: 0}, 300);
-    })
+    });
 });
 
 (function() {
-  // Create the first script tag for gtag.js
-  var gaScript = document.createElement('script');
-  gaScript.async = true;
-  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-HZPSTNML9Q';
-  document.head.appendChild(gaScript);
+    // Load Google Analytics script
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-HZPSTNML9Q';
+    document.head.appendChild(gaScript);
 
-  // Create and initialize the second script for Google Analytics setup
-  gaScript.onload = function() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', 'G-HZPSTNML9Q'); // Replace with your Measurement ID
-  };
+    // Initialize Google Analytics
+    gaScript.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-HZPSTNML9Q');
+    };
 })();
